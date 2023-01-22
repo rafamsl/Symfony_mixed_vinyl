e082b9fbb4a37ce5ba6254e69b2235b4a2e31aa1
rafamsl <rafael.msl81@gmail.com>
2023-01-22 20:21:17 +0100

    Added Twig templates

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,14 +2,29 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
-    public function homepage():Response{
-        return new Response('My first Symfony route');
+    public function homepage():Response
+    {
+
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey']
+        ];
+
+        return $this->render('vinyl/homepage.html.twig',[
+            'title' => 'Homepage',
+            'tracks' => $tracks
+        ]);
     }
     #[Route('/browse/{genre}')]
     public function browse(string $genre = null) : Response{
